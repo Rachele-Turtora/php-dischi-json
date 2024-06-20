bd875082a41ae1c1fbdd2ab94c004d604cf7d89a
Rachele-Turtora <racheleturtora21@gmail.com>
2024-06-20 14:56:41 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="app">
+        <header>
+            <div class="logo">
+                <img src="./img/logo.jpg" alt="logo">
+            </div>
+        </header>
+        <main>
+            <div class="container">
+
+            </div>
+        </main>
+    </div>
+
+    <!-- axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+    <!-- Vue.js -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+
+</html>
